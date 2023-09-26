70ef756cd8257f2343fd0bddc551d2737ae408ef
quinnna3643 <145154425+quinnna3643@users.noreply.github.com>
2023-09-26 14:20:12 -0400

    first code

---
+++ P1HW1_QuinnAllison.py
@@ -0,0 +1,14 @@
+# Firstname Lastname
+#9?26/202
+#Input, Output, expression
+
+num1 = int(input("Enter a number: "))
+
+print("You entered: ", num1)
+print(num1, "squared is ", num1**2)
+print("And", num1, "cubed is ", num1**3)
+
+num2 = int(input("Enter another number: "))
+
+print(num1, "+", num2, "=", num1+num2)
+print(num1, "*", num2, "=", num1*num2)
